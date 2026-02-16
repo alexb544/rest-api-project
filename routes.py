d16c326cb5f765289af14d37583a5ab82c1a9def
Alex <alexblake544@gmail.com>
2026-02-16 15:50:12 -0500

    worked more on routes.py functions

--- routes.py
+++ routes.py
@@ -8,47 +8,90 @@ router = APIRouter()
 def get_all_students():
     conn = get_connection()
     cursor = conn.cursor()
-    students = cursor.execute("SELECT * FROM students").fetchall()
+    results = cursor.execute("SELECT * FROM students").fetchall()
     conn.close()
-    return {"students": students, "count": len(students)}
+    return {"students": results, "count": len(results)}
 
 
-@router.get("???") 
+@router.get("/students/by-major", status_code=200) 
 def get_students_by_major(major: str):
     conn = get_connection()
     cursor = conn.cursor()
-    pass
+    results = cursor.execute("""
+        SELECT *
+        FROM students
+        WHERE major = ?""", (major,)
+    ).fetchall()
+
+    return {"students": results, "count": len(results), "major": major}
 
 
-@router.get("???") 
+@router.get("/students/by-gpa", status_code=200) 
 def get_students_by_gpa(min_gpa: float):
     conn = get_connection()
     cursor = conn.cursor()
-    pass
+
+    results = cursor.execute("""
+        SELECT *
+        FROM students
+        WHERE gpa > ?""", (min_gpa,)
+    ).fetchall()
+
+    conn.close()
+    return {"students": results, "count": len(results), "min_gpa": min_gpa}
 
 
-@router.get("???") 
+@router.get("/students/{student_id}", status_code=200) 
 def get_student(student_id: int):
     conn = get_connection()
     cursor = conn.cursor()
-    pass
 
+    result = cursor.execute("""
+        SELECT * 
+        FROM students 
+        WHERE id = ?""", (student_id,)
+    ).fetchone()   
+    conn.close()
+
+    if result is None:
+        raise HTTPException(status_code=404, detail=f"Student with ID {student_id} not found")
+    else:
+        return result
 
 @router.post("/students", status_code=201)
 def create_student(student: Student):
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO students VALUES (?, ?, ?, ?, ?, ?)", 
+    cursor.execute("""
+    INSERT INTO students
+        (id, name, email, major, gpa, enrollment_year) 
+    VALUES 
+        (?, ?, ?, ?, ?, ?)""", 
         (student.id, student.name, student.email, student.major, student.gpa, student.enrollment_year)
-    )
-    conn.commit()
+    )   
+    conn.commit() 
     conn.close()
 
-@router.put("???") 
+
+@router.put("/students/{student_id}", status_code=200) 
 def update_student(student_id: int, student: Student):
     conn = get_connection()
     cursor = conn.cursor()
-    pass
+
+    result = cursor.execute("""
+        UPDATE students 
+        SET student = ?
+        WHERE id = ?""", (student, student_id,)
+    )
+    conn.commit()
+    
+
+    conn.close()
+
+    if result is None:
+        raise HTTPException(status_code=404, detail=f"Student with ID {student_id} not found")
+    else:
+        return result
 
 
 @router.delete("/students/{student_id}") 
