4581e258e33872d56865e546dd2e9a3e8c2127ad
Alex <alexblake544@gmail.com>
2026-02-17 17:50:03 -0500

    finished input validation/error handling

--- routes.py
+++ routes.py
@@ -2,18 +2,25 @@ from fastapi import APIRouter, HTTPException
 from models import Student
 from database import get_connection
 
+
 router = APIRouter()
 
 @router.get("/students", status_code=200)
 def get_all_students():
+
     with get_connection() as conn:
         cursor = conn.cursor()
         results = cursor.execute("SELECT * FROM students").fetchall()
+
         return {"students": results, "count": len(results)}
 
 
-@router.get("/students/by-major", status_code=200) 
+@router.get("/students/by-major", status_code=200)
 def get_students_by_major(major: str):
+
+    if len(major) <= 0:
+        raise HTTPException(status_code=400, detail=f"GPA must be between 0.0 and 4.0")
+
     with get_connection() as conn:
         cursor = conn.cursor()
 
@@ -26,6 +33,7 @@ def get_students_by_major(major: str):
 
 @router.get("/students/by-gpa", status_code=200) 
 def get_students_by_gpa(min_gpa: float):
+
     if min_gpa < 0.0 or min_gpa > 4.0:
         raise HTTPException(status_code=400, detail=f"GPA must be between 0.0 and 4.0")
 
@@ -41,8 +49,10 @@ def get_students_by_gpa(min_gpa: float):
 
 @router.get("/students/{student_id}", status_code=200) 
 def get_student(student_id: int):
+
     with get_connection() as conn:
         cursor = conn.cursor()
+        
         result = cursor.execute("""
             SELECT * FROM students 
             WHERE id = ?""", (student_id,)).fetchone()
@@ -55,6 +65,7 @@ def get_student(student_id: int):
 
 @router.post("/students", status_code=201)
 def create_student(student: Student):
+
     with get_connection() as conn:
         cursor = conn.cursor()
         student_info = [student.name, student.email, student.major, student.gpa, student.enrollment_year]
@@ -76,6 +87,7 @@ def create_student(student: Student):
 
 @router.put("/students/{student_id}", status_code=200) 
 def update_student(student_id: int, student: Student):
+
     with get_connection() as conn:
         cursor = conn.cursor()
         new_student_info = [student.id, student.name, student.email, student.major, student.gpa, student.enrollment_year, student_id]
@@ -100,8 +112,10 @@ def update_student(student_id: int, student: Student):
 
 @router.delete("/students/{student_id}", status_code=200) 
 def delete_student(student_id: int):
+    
     with get_connection() as conn:
         cursor = conn.cursor()
+
         cursor.execute("""
             DELETE FROM students 
             WHERE id = ?
